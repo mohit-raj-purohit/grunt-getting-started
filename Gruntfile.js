8949267f84a5566c765a41142f3b1cb4e7f6f54d
Mohit Raj Purohit <mohit.raj.purohit@optimusinfo.com>
2016-02-28 02:56:00 +0530

    Create server and clean_build task register

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,10 @@ module.exports = function(grunt) {
 
         // configure watch to auto update ----------------
         watch: {
+            // Live Reload CSS/JS
+            // options: {
+            //     livereload: true
+            // },
 
             // for stylesheets, watch css and less files
             // only run less and cssmin
@@ -81,22 +85,64 @@ module.exports = function(grunt) {
                 files: 'src/**/*.js',
                 tasks: ['jshint', 'uglify']
             }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    hostname: '*',
+                    base: 'app',
+                    // Not Set keepalive to true.
+                    // Keepalive will block all subsequent tasks from running.
+                    // So long as connect is followed by watch the server won't terminate.
+                    // keepalive: true,
+                    open: {
+                        target: 'http://localhost:9000' // URL you want to run
+                    },
+                    // onCreateServer: function(server, connect, options) {
+                    //     console.log('Server Running');
+                    // }
+                }
+            }
         }
     });
 
     // ============= // CREATE TASKS ========== //
-    grunt.registerTask('preview', ['watch']);
+    grunt.registerTask('preview', ['connect', 'watch']);
+
+    // Remove build directory contents
+    grunt.registerTask('clean_build', function() {
+        grunt.file.delete('app/build/');
+        grunt.file.mkdir('app/build/');
+    });
 
     // ===========================================================================
     // LOAD GRUNT PLUGINS ========================================================
     // ===========================================================================
     // we can only load these if they are in our package.json
     // make sure you have run npm install so our app can find these
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-obfuscator');
+    // load npm tasks
+    var npmTasks = [
+        'grunt-contrib-jshint',
+        'grunt-contrib-uglify',
+        'grunt-contrib-less',
+        'grunt-contrib-cssmin',
+        'grunt-contrib-watch',
+        'grunt-obfuscator',
+        'grunt-contrib-connect'
+    ];
+
+    npmTasks.forEach(function(taskName) {
+        if(!grunt.task._tasks[taskName]) {
+            grunt.loadNpmTasks(taskName);
+        }
+    });
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-less');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-obfuscator');
+    // grunt.loadNpmTasks('grunt-contrib-connect');
 
 };
